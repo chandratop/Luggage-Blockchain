539d0038f70e0fbc9ca591386a8b883dc138eba2
AshishAsokan <raashish020@gmail.com>
2019-11-09 19:24:21 +0530

    Added test file

---
+++ test/luggage_test.js
@@ -0,0 +1,53 @@
+const Luggage = artifacts.require('./Luggage.sol')
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Luggage', ([deployer, seller, buyer]) => {
+    let luggage;
+
+    //Waiting for contract to be deployed
+    before(async() => {
+        luggage = await Luggage.deployed()
+    })
+
+    describe('deployment', async() => {
+
+        //Checking if the contract deploys sucessfully
+        it("contract deploys successfully", async() => {
+            //Reading the address of the deployed contract
+            const address = await luggage.address
+
+            //Checing if the address is undefined
+            assert.notEqual(address, '');
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+        })
+
+        it("contract has a name", async() => {
+            const name = await luggage.name();
+            assert.equal(name, "Luggage-Contract");
+        })
+    })
+    describe('product', async() => {
+        let new_request, count
+        before(async() => {
+            new_request = await luggage.createRequest("Abdul Habbibbullah", web3.utils.toWei('10', 'Ether'), [1, 0, 1, 1])
+            count = await luggage.requestCount()
+        })
+
+        it("Product created", async() => {
+            assert.equal(count, 1);
+            const request_id = new_request.logs[0].args
+            assert.equal(request_id.name, "Abdul Habbibbullah", "Name matches")
+            assert.equal(request_id.weight, web3.utils.toWei('10', 'Ether'), "Price matches")
+            assert.equal(request_id.expired, false, "Expiry state is same")
+
+            // FAILURE: Product must have a name
+            await await luggage.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+            // FAILURE: Product must have a price
+            await await luggage.createProduct('Vignesh', 0, { from: seller }).should.be.rejected;
+        })
+    })
+})
