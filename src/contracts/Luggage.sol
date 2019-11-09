bce7a995df5d9dac10687065a777aa45f18d3044
VigneshKKumar <vigneshkkumar127@gmail.com>
2019-11-09 20:21:12 +0530

    Changed implementation of acceptRequest

--- src/contracts/Luggage.sol
+++ src/contracts/Luggage.sol
@@ -5,7 +5,7 @@ contract Luggage {
     enum Categories {food,clothing,electronics,other}
     struct request {
         uint id;
-        address payable passenger;
+        address payable acceptor;
         string name;
         uint weight; // Assumes every bag is separate
         bytes4 categories;
@@ -14,7 +14,7 @@ contract Luggage {
 
     event requestCreated (
         uint id,
-        address payable passenger,
+        address payable acceptor,
         string name,
         uint weight,
         bytes4 categories,
@@ -23,7 +23,7 @@ contract Luggage {
 
     event requestAccepted (
         uint id,
-        address payable passenger,
+        address payable acceptor,
         string name,
         uint weight,
         bytes4 categories,
@@ -36,25 +36,25 @@ contract Luggage {
     constructor() public {
         name = "Luggage-Contract";
     }
-
     function createRequest(string memory _name,uint _weight,bytes4 _categories) public {
         require(bytes(_name).length > 0,"Invalid Name");
         require(_weight > 0,"Invalid Weight");
         requestCount ++;
-        requests[requestCount] = request(requestCount, msg.sender, _name,_weight,_categories,false);
-        emit requestCreated(requestCount, msg.sender, _name,_weight,_categories,false);
+        requests[requestCount] = request(requestCount, 0, _name,_weight,_categories,false);
+        emit requestCreated(requestCount,0, _name,_weight,_categories,false);
     }
 
-    function acceptRequest(uint _id) public payable {
+    function acceptRequest(uint _id,address payable _acceptor) public payable {
         request memory _request = requests[_id];        //Get the request
-        address payable _payer = _request.passenger;    //Get the person making the payment
+        address payable _payer = msg.sender;    //Get the person making the payment
+        _request.acceptor = _acceptor;
         require(_request.id > 0 && _request.id <= requestCount, "Invalid ID");
         require(msg.value >= _request.weight, "Invalid Value");
         require(!_request.expired, "Request already accepted");
-        require(_payer != msg.sender, "Requester is same as sender");
+        require(_acceptor != msg.sender, "Requester is same as sender");
         _request.expired = true;
         requests[_id] = _request;
-        address(msg.sender).transfer(msg.value);
+        address(_acceptor).transfer(msg.value);
         emit requestAccepted(requestCount, _payer, _request.name, _request.weight, _request.categories, _request.expired);
     }
 
