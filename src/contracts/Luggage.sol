38b020f5be2c3f7fea99212396b4edf4a3e18325
VigneshKKumar <vigneshkkumar127@gmail.com>
2019-11-09 19:16:24 +0530

    Reverted

--- src/contracts/Luggage.sol
+++ src/contracts/Luggage.sol
@@ -8,7 +8,6 @@ contract Luggage {
     struct request {
         uint id;
         address passenger;
-        string name;
         uint weight; // Assumes every bag is separate
         bytes4 categories;
         bool expired;
@@ -17,16 +16,6 @@ contract Luggage {
     event requestCreated (
         uint id,
         address passenger,
-        string name,
-        uint weight,
-        bytes4 categories,
-        bool expired
-    );
-
-    event requestAccepted (
-        uint id,
-        address passenger,
-        string name,
         uint weight,
         bytes4 categories,
         bool expired
@@ -43,21 +32,8 @@ contract Luggage {
         require(bytes(_name).length > 0,"Invalid Name");
         require(_weight > 0,"Invalid Weight");
         requestCount ++;
-        requests[requestCount] = request(requestCount, msg.sender, _name,_weight,_categories,false);
-        emit requestCreated(requestCount, msg.sender, _name,_weight,_categories,false);
-    }
-
-    function acceptRequest(uint _id) public payable {
-        request memory _request = requests[_id];        //Get the request
-        address payable _payer = _request.passenger;    //Get the person making the payment
-        require(_request.id > 0 && _request.id <= requestCount);
-        require(msg.value >= _request.weight); 
-        require(!_request.expired);
-        require(_payer != msg.sender);
-        _request.expired = true;
-        requests[_id] = _request;
-        address(msg.sender).transfer(msg.value)
-        emit requestAccepted()
+        requests[requestCount] = request(requestCount, msg.sender, _weight,_categories,false);
+        emit requestCreated(requestCount, msg.sender, _weight,_categories,false);
     }
 
 }
